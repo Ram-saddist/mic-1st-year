43e8cfcb1c1055d779ffcbd75289ff33116ea05c
Ram-saddist <nagulavancharavisivaramteja@gmail.com>
2025-01-26 11:23:44 +0530

    js fetch method

--- regular.js
+++ regular.js
@@ -1,8 +1,9 @@
 function validate(){
     var name=document.getElementById("name").value
     var email=document.getElementById("email").value
+    var city=document.getElementById("city").value
     var nameCheck=/^[a-zA-Z]+[a-zA-Z ]+$/
-    var emailCheck=/^[a-zA-Z$#]+@[a-z]+\.[a-z]{2,3}$/
+    var emailCheck=/^[a-zA-Z$#0-9]+@[a-z]+\.[a-z]{2,3}$/
                     //siva.ram  @codegnan.com
     console.log(name)
     if(name==""){
@@ -17,5 +18,32 @@ function validate(){
         alert("Check your email id once")
         return false
     }
+    if(city==""){
+        document.getElementById("city-error").innerHTML="Enter your city"
+        return false
+    }
     return true
 }
+
+/*
+Rest calls or server operations (CRUD actions)
+get         retrieve
+post        create
+put         update
+delete      delete
+
+using fetch() or axios 3rd part library we are going to access an API
+https://www.google.com/search?q=
+
+The response we get from an API or the request which we send is in JSON format
+JavaScript Object Notation(JSON)
+{
+    "key":value,
+    "key":value
+    .
+    .
+}
+
+*/
+
+
